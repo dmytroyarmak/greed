1bb503c5ff1427be69d0f0c8f7945003d4c3140c
Dima <0979029562@mail.ru>
2012-04-01 11:31:21 +0300

    some small changes

--- greed.rb
+++ greed.rb
@@ -17,23 +17,24 @@ class DiceSet
 	def roll(n)
 		@values.clear
 		n.times { @values << rand(6) + 1 }
+		score
 	end
 	def score
 		score = 0
 		no_valuable = @values.size
 		@values.group_by {|n| n}.each do |key, value|
-			len = value.length
-			if len >= 3
-				score += (key == 1)? 1000 : key * 100
-				len -= 3
+			quantity = value.length
+			if quantity >= 3
+				score += ( key == 1 ) ? 1000 : key * 100
+				quantity -= 3
 				no_valuable -= 3
 			end
 			if key == 5
-				score += len * 50
-				no_valuable -= len
+				score += quantity * 50
+				no_valuable -= quantity
 			elsif key == 1
-				score += len * 100
-				no_valuable -= len
+				score += quantity * 100
+				no_valuable -= quantity
 			end
 		end
 		no_valuable = 5 if no_valuable == 0
@@ -42,61 +43,57 @@ class DiceSet
 end
 
 class GreedGame
-	
-	def initialize(a_player_list)
-		raise ArgumentError, "Argument must be array of player" unless a_player_list.is_a?(Array) && a_player_list.size > 0
-		@player_list = a_player_list
+	def initialize(player_list)
+		raise ArgumentError, "Argument must be array of player" unless player_list.is_a?(Array) && player_list.size > 0
+		@player_list = player_list
 		@dice_set = DiceSet.new
 	end
-
 	def step_of_game(player)
-		current_score = 0
-		puts "\n#{player.name}! It's your step! Your score: #{player.score}"
-		number_of_rolls = 5
+		step_score = 0
+		puts "\n#{player.name}, It's your turn! Your score: #{player.score}"
+		number_of_throws = 5
 		while true
-			@dice_set.roll(number_of_rolls)
-			tmp_score, number_of_rolls = @dice_set.score
-			current_score += tmp_score
-			puts "\t#{player.name} roll #{@dice_set.values} (#{tmp_score}). Turn score: #{current_score}!"
-			if tmp_score == 0
-				puts "Fail!!! Zero-point roll."
+			throw_score, number_of_throws = @dice_set.roll(number_of_throws)
+			step_score += throw_score
+			puts "\t#{player.name} throw #{@dice_set.values} (#{throw_score}). Score of step: #{step_score}!"
+			if throw_score == 0
+				puts "Failure! Score of throw is zero."
 				break
 			end
-			if current_score >= 300	
-				print "\t\tYou have #{number_of_rolls} roll(s)! Continue? (y/n): "
-				answer = STDIN.gets
+			if step_score >= 300	
+				print "\t\tYou have #{number_of_throws} throw(s)! Continue? (y/n): "
+				answer = gets.downcase
 				if answer =~ /^n/
-					player.add_to_score(current_score)
-					puts "#{player.name} has #{player.score} points."
+					player.add_to_score(step_score)
+					puts "#{player.name} already has #{player.score} points."
 					break
 				end
 			end
 		end #while
 	end
-
-	def start
+	def play
 		puts "Game started"
 		already_have_above_3000 = nil
 		while already_have_above_3000.nil?
 			@player_list.each do |player|
 				step_of_game(player)
 				if player.score > 3000
-					puts "\n#{player.name} already have #{player.score} points!"
 					already_have_above_3000 = player
-					break
+					break #exit from each loop
 				end
-			end
-		end
-		# Final round
+			end #each
+		end #while
 		puts "\nFinal round"
-		@player_list.each { |player| step_of_game(player) if player != already_have_above_3000}
-		# Who won?
 		winner = already_have_above_3000
-		@player_list.each{ |player| winner = player if player.score > winner.score}
-		puts "\nСongratulation!!! #{winner.name} won with score: #{winner.score}"
+		@player_list.each do |player| 
+			if player != already_have_above_3000
+				step_of_game(player)
+				winner = player if player.score > winner.score
+			end 
+		end
+		puts "\nCongratulation!!! #{winner.name} win by a score #{winner.score}"
 	end
-
 end
 
 game = GreedGame.new([Player.new("Dima"), Player.new("Misha")])
-game.start
+game.play
